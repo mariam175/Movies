4733cf12f89420edea7fd6c772c6ff5dbc81f6c5
mariam175 <mariamrafaat28@gmail.com>
2025-05-13 14:42:35 +0300

    add nib file

--- movies/moviesTableViewCell.swift
+++ movies/moviesTableViewCell.swift
@@ -1,30 +1,30 @@
 //
-//  TableViewCell.swift
+//  moviesTableViewCell.swift
 //  movies
 //
-//  Created by Macos on 03/05/2025.
+//  Created by Macos on 13/05/2025.
 //
 
 import UIKit
 
-class TableViewCell: UITableViewCell {
+class moviesTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var movieImage: UIImageView!
-    @IBOutlet weak var movieLabel: UILabel!
-    var movie : MoviesApi? = nil
-    var isFav : Bool = false
     @IBOutlet weak var favBtn: UIButton!
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var movieLabel: UILabel!
+    var movie : MoviesApi? = nil
+    var isFav : Bool = false
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
 
-    override func layoutSubviews() {
-       
+        // Configure the view for the selected state
     }
     
-    @IBAction func addTofavs(_ sender: Any) {
+    @IBAction func addToFav(_ sender: Any) {
         if let storedMovie = movie{
             if isFav {
                 FavsDB.instance.deleteMovie(title: storedMovie.Title ?? "")
@@ -35,13 +35,6 @@ class TableViewCell: UITableViewCell {
                 favBtn.setImage(UIImage(systemName: "heart.fill"), for: .normal)
             }
         }
-      
-        
     }
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
 }
